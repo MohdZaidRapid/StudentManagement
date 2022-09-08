8ec07caa723808568e288d5e01c4613c0eef09c5
mohdzaid <mohdzaid@gmail.com>
2022-09-08 15:32:22 +0530

    added helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const options = new DocumentBuilder()
